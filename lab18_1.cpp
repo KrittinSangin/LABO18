6bd303d395b34e3b0a20cd18dfc13d4d3d57520c
Perm <permsangin@gmail.com>
2025-02-10 00:23:10 +0700

    Finished all lab

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,38 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect &R1, Rect &R2){
+	double minx,maxx,miny,maxy;
+	if(R1.x <= R2.x){
+		minx = R2.x;
+	}else{
+		minx = R1.x;
+	}
+	if((R2.x+R2.w) >= (R1.x+R1.w)){
+		maxx = (R1.x+R1.w);
+	}else{
+		maxx = (R2.x+R2.w);
+	}
+	double deltax = maxx - minx;
 
 
+	if(R1.y <= R2.y){
+		miny = R1.y;
+	}else{
+		miny = R2.y;
+	}
+	if((R2.y-R2.h) <= (R1.y+R1.h)){
+		maxy = (R2.y-R2.h);
+	}else{
+		maxy = (R1.y-R1.h);
+	}
+	
+	double deltay = maxy - miny;
+    if(deltay < 0)deltay = - deltay;
+	
 
+
+    if((R1.x + R1.w) < (R2.x))return 0;
+
+	return deltax*deltay;
 }
--- lab18_3.cpp
+++ lab18_3.cpp
@@ -8,7 +8,10 @@
 using namespace std;
 
 struct student{
-
+    char name[50] = {};
+    int id;
+    char gender;
+    double gpa;
     //Define struct student with four member (name ,id , gender, gpa);
     
 };
@@ -20,7 +23,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student > &allstudents,int key){ //There is something wrong in this line.
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -63,7 +66,13 @@ int main(){
 	
 	while(getline(student_file,textline)){
 		student s; 
-		
+		char name[50];
+		//ชื่อ รหัสนักศึกษา เพศ และเกรดเฉลี่ย Monkey D. Luffy,1001,M,1.45
+		sscanf(textline.c_str(),"%[^,],%d,%c,%le",name,&s.id,&s.gender,&s.gpa);
+		for (int i = 0; i < 50; i++)
+		{
+			s.name[i] = name[i];
+		}
 		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
 
 		allstudents.push_back(s); 		
@@ -83,7 +92,8 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
+				allcourses[allcourses.size()-1].lecture_list.push_back(textline);
+
 			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
 			    
 			}			
@@ -92,7 +102,7 @@ int main(){
 				state = 1;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
+				allcourses[allcourses.size()-1].student_list.push_back(p);
 				//Append (push_back) p to student_list of the recently added course in allcourses[];
 				
 			}
